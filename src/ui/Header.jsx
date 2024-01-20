15d5e9e9f3dfd430d44b5ee5c3f34583e3b0d021
John Bryan Odina <odeng.odia1122@gmail.com>
2024-01-20 20:06:01 +0800

    Edit of header and favicon

--- src/ui/Header.jsx
+++ src/ui/Header.jsx
@@ -2,15 +2,16 @@ import styled from "styled-components";
 import PayrollMonth from "./PayrollMonth";
 
 const StyledHeader = styled.header`
-  background-color: var(--color-tertiary-900);
+  background-color: var(--color-tertiary-700);
   padding: 1.2rem 4.8rem;
+  color: var(--color-primary-900);
   border-bottom: 1px solid var(--color-grey-100);
   @media (max-width: 600px) {
     font-size: 1rem;
     padding: 1.2rem 3rem;
   }
   h2 {
-    color: var(--color-grey-500);
+    color: var(--color-grey-200);
   }
 `;
 
